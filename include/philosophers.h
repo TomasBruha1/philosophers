53f4f464a9a2d6fbb052eac3d6927470c35c8c5e
Tomas Bruha <tomasbruha1@gmail.com>
2025-02-13 16:22:57 +0100

    Let's not die philos

---
+++ include/philosophers.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbruha <tbruha@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/13 15:08:33 by tbruha            #+#    #+#             */
+/*   Updated: 2025/02/13 15:15:10 by tbruha           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+// HEADERS
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdbool.h>
+
+// STRUCTS
+
+// FUNCTIONS
+
+// philosophers.c
+
+#endif
---
+++ instructions.md
@@ -0,0 +1,3 @@
+13.2.2025
+Lets put instructions and other stuff here once it gets crowded with the code.
+
